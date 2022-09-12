42d4baf321c3c243808c04f45ddbdd0fba8fdbbc
inqueen <inqueenofficial@gmail.com>
2022-09-12 20:26:11 -0300

    added class #1

--- python_class1.py
+++ python_class1.py
@@ -1,7 +1,14 @@
 # Databricks notebook source
+# %%
 print("hello, queen")
 
 # COMMAND ----------
 
 # DBTITLE 1,Variables
+# %%
 name = "Queen"
+type(name)
+# %%
+
+name = input("Digite seu nome: ")
+#%%
